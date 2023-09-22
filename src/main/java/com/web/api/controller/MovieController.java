0fe881eb3ea3a08c43751dc63d8ab029bf87d2ad
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-09-22 15:52:43 +0700

    Small refactor

--- src/main/java/com/web/api/controller/MovieController.java
+++ src/main/java/com/web/api/controller/MovieController.java
@@ -8,16 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/movies")
 public class MovieController {
 
-    private final List<Movie> movieList;
+    List<Movie> movieList = new ArrayList<>();
 
     public MovieController() throws ParseException {
-        movieList = DbSeeder.getMovieList();
+        movieList.addAll(DbSeeder.getMovieList());
     }
 
     @GetMapping
@@ -26,7 +27,7 @@ public class MovieController {
     }
 
     @GetMapping("/{movie_id}")
-    public Movie getMovie(@PathVariable("movie_id") int movieId){
+    public Movie getMovie(@PathVariable("movie_id") int movieId) {
         return movieList.stream()
                 .filter(movie -> movie.id() == movieId)
                 .findAny()
