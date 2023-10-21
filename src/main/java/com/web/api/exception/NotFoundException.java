adc795d7d149f64a7949d5a6ce1cb662ada2a2b9
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-10-21 13:04:45 +0700

    Created NotFoundException

---
+++ src/main/java/com/web/api/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.web.api.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
