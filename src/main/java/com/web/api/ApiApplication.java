eb7db5a4318e87661ed5173e8d48eb1500e53668
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-10-21 14:24:47 +0700

    Imported resources from beans

--- src/main/java/com/web/api/ApiApplication.java
+++ src/main/java/com/web/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.web.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"classpath*:beans.xml"})
 public class ApiApplication {
 
     public static void main(String[] args) {
