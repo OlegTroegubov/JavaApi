7d9791fe93f3d5d3af7750c15d44acd485ac9535
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-10-21 12:48:57 +0700

    Deleted ctors from record

--- src/main/java/com/web/api/model/Movie.java
+++ src/main/java/com/web/api/model/Movie.java
@@ -1,13 +1,8 @@
 package com.web.api.model;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public record Movie(int id, int producerId, String title, Date releaseDate, float movieRating, boolean isTopRanked, MovieGenre genre){
-
-    public Movie(int id, int producerId, String title, String releaseDate, float movieRating, boolean isTopRanked, MovieGenre genre) throws ParseException {
-        this(id, producerId, title, new SimpleDateFormat("dd.MM.yyyy").parse(releaseDate), movieRating, isTopRanked, genre);
-    }
+public record Movie(int movieId, int producerId, String title, Date releaseDate, float movieRating, boolean isTopRanked,
+                    MovieGenre genre) {
 }
 
