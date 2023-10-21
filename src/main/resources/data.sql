24f12da7ef2e73553b2022fa4e59b9bf264a5d25
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-10-21 12:56:52 +0700

    Created db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO PRODUCERS
+VALUES (1, 'Ivan', 'Ivanov', '1999-01-23', 'ivan@gmail.com', true);
+
+INSERT INTO PRODUCERS
+VALUES (2, 'Oleg', 'Francis', '1950-10-23', 'alex@gmail.com', true);
+
+INSERT INTO MOVIES
+VALUES (1, 1, 'Sherlock', '2011-02-03', 7.5, true, 'ACTION');
+
+INSERT INTO MOVIES
+VALUES (2, 1, 'Soul', '2023-10-24', 3.5, false, 'DRAMA');
+
+INSERT INTO MOVIES
+VALUES (3, 1, 'Another Movie', '2005-12-15', 8.0, true, 'COMEDY');
+
+INSERT INTO MOVIES
+VALUES (4, 2, 'Action Flick', '2010-08-20', 6.9, false, 'ACTION');
+
+INSERT INTO MOVIES
+VALUES (5, 2, 'Dramatic Tale', '2015-05-10', 7.2, true, 'DRAMA');
+
+INSERT INTO MOVIES
+VALUES (6, 2, 'Comedy Night', '2008-11-05', 7.8, false, 'COMEDY');
+
